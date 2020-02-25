55feef21505018f5c9880855108f4afcea845a0a
Fifilutfiah1282 <lutfiahahaa@gmail.com>
2020-02-25 14:54:54 +0700

    add examp1

---
+++ exam1/fifi-livecode-exam1/lifecode.rb
@@ -0,0 +1,21 @@
+lemari = ["Buku","Handpone","Pensil","Baju", "Flashdrive"]
+tas = []
+tas = lemari [0], lemari [2], lemari [4]
+puts "=====lemari======"
+puts lemari[1], lemari [3]
+puts"======tas========"
+puts tas
+ 
+
+# puts "====== array 1======"
+# array1 = [1,2,3,4,5,6,7,8,9]
+# array2 = [1,7,6,0]
+
+# array1.pop(3)
+# array1.unshift(7,8,9)
+# puts array1
+
+# puts "==== array 2====="
+# array2.shift(1)
+# array2.insert(3,1)
+# puts array2
