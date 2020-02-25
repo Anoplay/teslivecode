9fc1021767c808f814e36b3393b2189716c03184
irmaliani <irmaliaanii17@gmail.com>
2020-02-25 14:18:44 +0700

    add exam1

---
+++ exam1/irmaliani-livecode-exam1/pertama.rb
@@ -0,0 +1,8 @@
+lemari = ["Buku","Handphone","Pensil","Baju","Flashdrive"]
+tas = []
+
+tas = lemari [0], lemari[2], lemari[4]
+puts "===============lemari======================="
+puts lemari[1], lemari[3]
+puts "===============tas=========================="
+puts tas
