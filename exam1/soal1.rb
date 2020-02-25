1d43b0023d75be1c14d8d56c193725469ee09554
Muchtarom <muhtarom091@gmail.com>
2020-02-25 14:57:07 +0700

    add exam1

---
+++ exam1/soal1.rb
@@ -0,0 +1,9 @@
+puts "Soal No.1"
+
+lemari =["Buku","Handphone","Pensil","Baju","Flashdrive"]
+tas =[]
+puts "================== Lemari ==================="
+puts "[#{lemari [1]}, #{lemari [3]}]"
+puts "=================== Tas  ===================="
+puts tas,"[#{lemari [0]}, #{lemari [2]}, #{lemari [4]}]"
+puts " "
---
+++ exam1/soal2.rb
@@ -0,0 +1,13 @@
+puts "Soal No.2"
+array1 =[1,2,3,4,5,6,7,8,9]
+array2 =[1,7,6,0]
+puts "================== array1 ================="
+array1.pop (3)
+array1.unshift(7,8,9)
+print array1
+puts " "
+puts "================== array2  ================"
+array2.shift(1)
+array2.insert(3,1)
+print array2
+puts " "
