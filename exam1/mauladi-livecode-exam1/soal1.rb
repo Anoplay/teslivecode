b193540772e2e156861f4dbcfdc6721a5b35bd7d
mauladi231 <mauladiarifiqbal@gmail.com>
2020-02-25 14:29:55 +0700

    mauladi-examl

---
+++ exam1/mauladi-livecode-exam1/soal1.rb
@@ -0,0 +1,8 @@
+lemari = ["buku","handphone","pensil","baju","flashdrive"]
+tas = []
+
+puts "===== Lemari ====="
+puts "#{lemari[1]}, #{lemari[3]}"
+
+puts "===== Tas ====="
+puts tas,"#{lemari[0]}, #{lemari[2]}, #{lemari[4]}"
