4005127d28ef6a4a504bc312280f36080ffd6727
eneng75 <enengrita75@gmail.com>
2020-02-25 14:14:04 +0700

    add exam1

---
+++ exam1/livecode/soal2.rb
@@ -0,0 +1,7 @@
+lemari = ["Buku","Handphone","Pensil","Baju","Flashdrive"]
+tas = []
+tas = lemari [0], lemari [2], lemari [4]
+puts "--------Lemari-------"
+puts lemari[1], lemari[3]
+puts "---------tas--------"
+puts tas
